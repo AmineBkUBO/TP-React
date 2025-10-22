2b8b490486aec8d98a883141208778cad71e5239
AmineBkUBO <aminejustagamer@gmail.com>
2025-10-22 14:32:16 +0200

    added routes and store

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import {Login} from "./user/Login";
 
 function App() {
 
   return (
-      <Login/>
+      <Routes>
+          <Route path="*" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+      </Routes>
   );
 }
 
